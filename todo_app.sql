c9ae1a2e3875eb5f0a3de170bc81b783b0630e8c
Edward Gao <egao@sent.com>
2017-01-19 18:29:37 -1000

    step 6

--- todo_app.sql
+++ todo_app.sql
@@ -3,3 +3,11 @@ CREATE USER "michael";
 DROP DATABASE IF EXISTS "todo_app";
 CREATE DATABASE "todo_app";
 \connect todo_app;
+CREATE TABLE "tasks" ( 
+	id serial NOT NULL PRIMARY KEY,
+	title VARCHAR(255) NOT NULL,
+	description text NULL,
+	created_at timestamp NOT NULL,
+	updated_at timestamp NULL,
+	complete boolean NOT NULL
+);
